a9e936434ccabf30c74c915d2c654d965d98d958
Malay Patel <mpate432@uwo.ca>
2022-12-07 18:41:45 -0500

    tracks and artist

--- client/src/actions/tracks.js
+++ client/src/actions/tracks.js
@@ -1,13 +1,14 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, END_LOADING } from '../constants/actionTypes';
 
 import * as api from '../api/index.js';
+import Axios from 'axios';
 
 export const getTracks = () => async (dispatch) => {
   try {
-    
     const { data } = await api.fetchTracks();
     dispatch({ type: END_LOADING});
     dispatch({ type: FETCH_ALL, payload: data });
+    console.log(data)
 
   } catch (error) {
     console.log(error.message);
--- client/src/components/Artist/Artist.js
+++ client/src/components/Artist/Artist.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getTracks } from '../../actions/tracks';
+import { getArtists } from '../../actions/artist';
 
-const Tracks = () => {
+const Artists = () => {
     const [currentId, setCurrentId] = useState(0);
     const dispatch = useDispatch();
     
     const query = useQuery();
     const history = useHistory();
     const classes = useStyles();
-    
+
     const [search, setSearch] = useState('');
   
     useEffect(() => {
-      dispatch(getTracks());
+      dispatch(getArtists());
     }, [currentId, dispatch]);
 
+
+    
 
 
     return (
       <h1> </h1>
     )}
 
-export default Track;
+export default Artists;
